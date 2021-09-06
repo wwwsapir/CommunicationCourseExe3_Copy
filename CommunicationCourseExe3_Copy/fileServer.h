a3f5b5fbc7557e34987035424101a5a3ec5c035f
Pavel Sheinfeld <pavel.sheinfeld@gmail.com>
2021-09-06 10:33:30 +0300

    v2

--- CommunicationCourseExe3_Copy/fileServer.h
+++ CommunicationCourseExe3_Copy/fileServer.h
@@ -2,7 +2,7 @@
 #define FILE_ERROR -1
 #define SUCCESS 0
 
-int getFileObject(char* path, char** filelContentPtr, int* contentLenPtr);
+int getFileObject(char* path, char** filelContentPtr, int* contentLenPtr); //f1
 int createFileObject(char* path, char* newFileContent);
 int deleteFileObject(char* path);
 void getLastModifiedDate(char* path, char* buffer);
